7563727b7500fdc0b77922dc89956d3669f13e29
yuta-nishina <y-nishina@atgs.jp>
2016-05-30 13:04:15 +0900

    タブで切り替えていたアクティビティをフラグメント化
    ※公式リンクボタンを押すとエラーがでる

--- proj.android-studio/app/src/org/cocos2dx/cpp/MainActivity.java
+++ proj.android-studio/app/src/org/cocos2dx/cpp/MainActivity.java
@@ -1,20 +1,24 @@
 package org.cocos2dx.cpp;
 
+/**
+ * Created by arimatakeshi on 16/05/16.
+ */
+
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
-
-    private Intent intentBgm = null;
+public class MainActivity extends Activity{
 
     @Override
-    // 初期表示
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -22,26 +26,60 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        // フォント変更
-        TextView txt = (TextView) findViewById(R.id.splashLogo);
-        txt.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/logotypejp_mp_m_1.1.ttf"));
-
-        // サービスクラスを指定
-        intentBgm = new Intent(this, BgmService.class);
-        startService(intentBgm);
-
-        // スプラッシュ
-        Handler handler = new Handler();
-        handler.postDelayed(new splashHandler(), 3000);
     }
 
-    // 自動画面遷移
-    class splashHandler implements Runnable {
-        public void run() {
-            Intent intentNext = new Intent(getApplication(), HomeActivity.class);
-            startActivity(intentNext);
-            MainActivity.this.finish();
+    // タブメニュー操作
+    public void tabClick(View view){
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        switch (view.getId()){
+
+            case R.id.homeBtn:
+                HomeFragment homeFragment = new HomeFragment();
+                transaction.replace(R.id.fragment_container,homeFragment);
+                transaction.commit();
+
+                break;
+
+            case R.id.characterBtn:
+                CharacterSelectFragment characterSelectFragment = new CharacterSelectFragment();
+                transaction.replace(R.id.fragment_container,characterSelectFragment);
+                transaction.commit();
+
+                break;
+
+            case R.id.settingBtn:
+                SettingFragment settingFragment = new SettingFragment();
+                transaction.replace(R.id.fragment_container,settingFragment);
+                transaction.commit();
+                break;
+
+            default:
+                break;
+
         }
     }
 
+    /*
+    * フォントタイプ指定
+    * setFontType()
+    */
+    public void setFontType(TextView txt) {
+        txt.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/logotypejp_mp_m_1.1.ttf"));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // TODO 閉じる時に音を消す処理
+        Log.i("tabactivity", "閉じたよー");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // TODO 閉じる時に音を消す処理
+        Log.i("tabactivity", "閉じたよーdestory");
+    }
 }
---
+++ proj.android-studio/app/src/org/cocos2dx/cpp/SettingFragment.java
@@ -0,0 +1,125 @@
+package org.cocos2dx.cpp;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class SettingFragment extends Fragment {
+
+    private Intent intentBgm = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_setting,container,false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // アクティビティのメソッドを使うためにインスタンス化
+        MainActivity activity = (MainActivity) getActivity();
+
+        // フォント変更
+        TextView txt = (TextView)activity.findViewById(R.id.volumeLbl);
+        activity.setFontType(txt);
+
+        // フォント変更
+        TextView optTxt = (TextView)activity.findViewById(R.id.optionText);
+        activity.setFontType(optTxt);
+
+        // フォント変更
+        TextView bgmTxt = (TextView)activity.findViewById(R.id.bgmTxt);
+        activity.setFontType(bgmTxt);
+
+        // フォント変更
+        TextView seTxt = (TextView)activity.findViewById(R.id.seTxt);
+        activity.setFontType(seTxt);
+
+
+        // ボリューム取得
+        final SeekBar vol = (SeekBar)activity.findViewById(R.id.volumeBar);
+
+        AudioManager am = (AudioManager)activity.getSystemService(Context.AUDIO_SERVICE);
+
+        // 初期値
+        vol.setProgress(am.getStreamVolume(AudioManager.STREAM_MUSIC));
+        // 最大値
+        vol.setMax(15);
+
+        // シークバーコントロール
+        vol.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progress, boolean fromUser) {
+                    }
+
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        // ツマミに触れたときに呼ばれる
+                    }
+
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        // ツマミを離したときに呼ばれる
+                        Log.i("debug","out" + seekBar.getProgress());
+                        AudioManager am = (AudioManager)getActivity().getSystemService(Context.AUDIO_SERVICE);
+                        am.setStreamVolume(AudioManager.STREAM_MUSIC, seekBar.getProgress(), 0);
+                    }
+                }
+        );
+
+        ToggleButton s = (ToggleButton)activity.findViewById(R.id.bgmSwitch);
+
+        if (s != null) {
+            s.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
+                    // サービスクラスを指定
+                    intentBgm = new Intent(getActivity(), BgmService.class);
+
+                    if(isChecked) {
+                        //do stuff when Switch is ON
+                        Log.i("debug","開始");
+                        getActivity().startService(intentBgm);
+
+
+                    } else {
+                        //do stuff when Switch if OFF
+                        Log.i("debug","停止");
+                        getActivity().stopService(intentBgm);
+
+                    }
+                }
+            });
+        }
+    }
+
+    // ボタン操作
+    public void onClick(View view){
+        switch (view.getId()){
+
+            case R.id.sitelink:
+                Uri uri = Uri.parse("https://www.google.co.jp");
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(i);
+                break;
+
+        }
+    }
+
+
+}
