ab8dd85e63146d6e98b4de904da88361048d486a
yuta-nishina <y-nishina@atgs.jp>
2016-06-07 11:23:25 +0900

    ゲーム画面からホーム画面に戻る処理を追加

---
+++ Classes/TitleScene.mm
@@ -0,0 +1,112 @@
+//
+//  TitleScene.cpp
+//  SaveTheBird
+//
+//  Created by giginet on 2014/8/1.
+//
+//
+
+#include "TitleScene.h"
+#include "MainScene.h"
+#include "SimpleAudioEngine.h"
+#include "AudioUtils.h"
+
+USING_NS_CC;
+
+Scene* TitleScene::createScene()
+{
+    auto scene = Scene::create();
+    auto layer = TitleScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+TitleScene::TitleScene()
+{
+}
+
+TitleScene::~TitleScene()
+{
+}
+
+bool TitleScene::init()
+{
+    if (!Layer::init()) {
+        return false;
+    }
+    
+    auto winSize = Director::getInstance()->getWinSize();
+    auto background = Sprite::create("title.png");
+    background->setPosition(Vec2(winSize.width / 2.0, winSize.height / 2.0));
+    this->addChild(background);
+    
+//    auto start = Sprite::create("start.png");
+//    this->addChild(start);
+//    start->setPosition(Vec2(winSize.width / 2, winSize.height / 4));
+//    
+//    // 画面をタッチしたときにメイン画面へ遷移
+//    auto listener = EventListenerTouchOneByOne::create();
+//    listener->onTouchBegan = [this, start](Touch* touch, Event* event) {
+//        start->setTexture(Director::getInstance()->getTextureCache()->addImage("start_pressed.png"));
+//        CocosDenshion::SimpleAudioEngine::getInstance()->playEffect(AudioUtils::getFileName("start").c_str());
+//        this->getEventDispatcher()->removeAllEventListeners();
+//        
+//        this->runAction(Sequence::create(DelayTime::create(1.0),
+//                                         CallFunc::create([] {
+//            auto main = MainScene::createSceneWithStage(0);
+//            auto transition = TransitionCrossFade::create(0.5, main);
+//            
+//            Director::getInstance()->replaceScene(transition);
+//        }), NULL));
+//        
+//        return true;
+//    };
+////    listener->onTouchEnded = [this](Touch* touch, Event* event) {
+////        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+////        UIViewController *initialViewController = [storyboard instantiateInitialViewController];
+////        
+////        UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+////        [controller presentViewController:initialViewController animated:YES completion:nil];
+////        Director::getInstance()->end();
+////    
+////    };
+//    
+//    this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+//
+    
+    auto returnHome = MenuItemImage::create("return.png", "return_pressed.png", [this](Ref *sender){
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+        UIViewController *initialViewController = [storyboard instantiateInitialViewController];
+        
+        UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [controller presentViewController:initialViewController animated:YES completion:nil];
+        Director::getInstance()->end();
+    });
+    
+    auto startGame = MenuItemImage::create("start.png", "start_pressed.png", [this](Ref *sender){
+                CocosDenshion::SimpleAudioEngine::getInstance()->playEffect(AudioUtils::getFileName("start").c_str());
+                this->getEventDispatcher()->removeAllEventListeners();
+        
+                this->runAction(Sequence::create(DelayTime::create(1.0),
+                                                 CallFunc::create([] {
+                    auto main = MainScene::createSceneWithStage(0);
+                    auto transition = TransitionCrossFade::create(0.5, main);
+        
+                    Director::getInstance()->replaceScene(transition);
+                }), NULL));
+    });
+    
+    auto menu = Menu::create(startGame, returnHome, nullptr);
+    menu->alignItemsVerticallyWithPadding(10);
+    
+    background->addChild(menu);
+    
+    
+    return true;
+}
+
+void TitleScene::onEnterTransitionDidFinish()
+{
+    // BGMの再生
+    //CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic(AudioUtils::getFileName("title").c_str(), true);
+}
